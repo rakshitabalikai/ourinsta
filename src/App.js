719a26519d6d57d844d13d291505a5d74f855c16
rakshitabalikai <rakshitabalikai09@gmail.com>
2024-09-21 21:29:49 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/pages/Home';
+import Login from './components/pages/Login';
 
 function App() {
   return (
     <div>
       <Home></Home>
+      <Login></Login>
     
       
     </div>
