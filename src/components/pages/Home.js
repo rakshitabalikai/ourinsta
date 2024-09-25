7afe0397d0759df72eb4714fbba96cf739a1bbae
PREETHINRPREETHINR <preethinr07@gmail.com>
2024-09-24 23:49:41 -0400

    home page updated

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,13 @@
 import '../css/Home.css'
 import home from '../assets/icons/home.png';
-import search from '../assets/icons/search.png'
-import explore from '../assets/icons/explore.png'
-import reels from '../assets/icons/reels.png'
-import messages from '../assets/icons/messages.png'
-import notifications from '../assets/icons/notifications.png'
-import create from '../assets/icons/create.png'
-import profile from '../assets/icons/profile.png'
-import hamburger from '../assets/icons/hamburger.png'
-
+import search from '../assets/icons/search.png';
+import explore from '../assets/icons/explore.png';
+import reels from '../assets/icons/reels.png';
+import messages from '../assets/icons/messages.png';
+import notifications from '../assets/icons/notifications.png';
+import create from '../assets/icons/create.png';
+import profileIcon from '../assets/icons/profile.png';
+import hamburger from '../assets/icons/hamburger.png';
 
 
 const posts = [
@@ -18,8 +17,11 @@ const posts = [
 
 function Home(){
     return(
-        <div className='Homecontainer'>
-                <div className="menu">
+        <div className="home-container">
+      {/* Sidebar */}
+      <div className="sidebar">
+        <h1>Instagram</h1>
+        <ul>
         <button className='menu-button'><img src={home} alt="Home" /> Home</button>
         <button className='menu-button'><img src={search} alt="Search" /> Search</button>
         <button className='menu-button'><img src={explore} alt="Explore" /> Explore</button>
@@ -27,27 +29,46 @@ function Home(){
         <button className='menu-button'><img src={messages} alt="Messages" /> Messages</button>
         <button className='menu-button'><img src={notifications} alt="Notifications" /> Notifications</button>
         <button className='menu-button'><img src={create} alt="Create" /> Create</button>
-        <button className='menu-button'><img src={profile} alt="Profile" /> Profile</button>
+        <button className='menu-button'><img src={profileIcon} alt="Profile" /> Profile</button>
         <button className='menu-button'><img src={hamburger} alt="More" /> More</button>
-      </div>    
-            <div className='homesection2'>
+        </ul>
+      </div>
 
+      {/* Main Feed */}
+      <div className="main-feed">
+        <div className="stories">
+          <div className="story">Story 1</div>
+          <div className="story">Story 2</div>
+          <div className="story">Story 3</div>
+          <div className="story">Story 4</div>
+        </div>
+
+        <div className="post">
+          <div className="post-header">
+            <div className="profile-picture"></div>
+            <div className="profile-info">
+              <p>tartinebakery</p>
+              <span>San Francisco, California</span>
             </div>
-            <div className='homesection3'>
-                <div className='homesection3_1'>
-                    <button type="button" className='profilebutton'><img src={profile}></img>Profile</button>
-                    <button type="button" className='switch'>Switch</button>
-                </div>
-                <div className='homesection3_2'>
-                    <div className='homesection3_1'>
-                    <h3 className='suggforu'>Suggested for you</h3>
-                    <button type="button" className='switch'>see all</button>
-                    </div>
-                </div>
-  
-            </div>
-        
-</div>  
+          </div>
+          <div className="post-image">
+            <img src="your_image_url_here" alt="Pie" />
+          </div>
+        </div>
+      </div>
+
+      {/* Suggestions */}
+      <div className="suggestions">
+        <p>Suggestions For You</p>
+        <ul>
+          <li>alex.anyways18 - Follow</li>
+          <li>chantoulflowergirl - Follow</li>
+          <li>gwangu77 - Follow</li>
+          <li>mishka_songs - Follow</li>
+          <li>pierre_thecomet - Follow</li>
+        </ul>
+      </div>
+    </div>
     );
 }
 export default Home;
