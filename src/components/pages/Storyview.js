57f77b293ea1ab8d95fce759a0569620f5bf300c
PREETHINRPREETHINR <preethinr07@gmail.com>
2024-10-05 04:05:11 -0400

    storyview

---
+++ src/components/pages/Storyview.js
@@ -0,0 +1,26 @@
+// StoryView.jsx
+import React from 'react';
+import '../css/Storyview.css';
+
+const StoryView = ({ user, story, closeStory }) => {
+  return (
+    <div className="story-view-container">
+      <div className="story-header">
+        <div className="user-info">
+          <img className="user-avatar" src={user.profilePicture} alt={user.name} />
+          <div>
+            <p className="user-name">{user.name}</p>
+            <p className="time-posted">{story.timeAgo} ago</p>
+          </div>
+        </div>
+        <button onClick={closeStory} className="close-btn">X</button>
+      </div>
+
+      <div className="story-content">
+        <img src={story.content} alt="story" className="story-image" />
+      </div>
+    </div>
+  );
+};
+
+export default StoryView;
