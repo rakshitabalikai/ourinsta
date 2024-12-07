7e7ee0ef3d430a92c530426fbcdd8d44ec3f8ce3
puneeth037h <punithshanakanahalli@gmail.com>
2024-12-07 21:36:16 +0530

    like icon edited

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -191,13 +191,13 @@ function Home() {
                         <div class="container">
                         <button className="menu-button" onClick={() => handleLike(post._id)}><img src={like} alt="Like" /></button>
                             <input type="text" name="text" class="input" required="" placeholder="Type to search..."></input>
-                            <div class="icon">
+                            {/* <div class="icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512">
                                     <title>Search</title>
                                     <path d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"></path>
                                     <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32" d="M338.29 338.29L448 448"></path>
                                 </svg>
-                            </div>
+                            </div> */}
                         </div>                    
                     
                       
