aab403b896798fd893229392ed803817b8198cd4
PREETHINRPREETHINR <preethinr07@gmail.com>
2024-12-10 01:15:46 -0500

    aboutus

--- src/components/pages/Aboutus.js
+++ src/components/pages/Aboutus.js
@@ -30,30 +30,34 @@ function about_us() {
           socially and academically.
         </p>
       </div>
-
+      <h2 className='contact'>Contact Us</h2>
+      <p className='ct-script'>Have questions or feedback? Connect with the people behind Connectify!</p>
       <div className='contact-us-container'>
-        <h2 className='contact'>Contact Us</h2>
-        <p className='ct-script'>Have questions or feedback? Connect with the people behind Connectify!</p>
-        <ul className='images'>
-          
-            <img className='img-1' src="https://via.placeholder.com/100" alt="Preethi" />
-            <h1 className='n1'>Preethi:Reach out at </h1>
-            <a className='e1' href="mailto:preethi@connectify.com"> preethi@connectify.com</a>
-            
-         
-            <img className='img-2' src="https://via.placeholder.com/100" alt="Puneeth" />
-            <h1 className='n2'>Puneeth: Contact at </h1>
-            <a  className='e2' href="mailto:puneeth@connectify.com"> puneeth@connectify.com</a>.
-         
-            <img className='img-3' src="https://via.placeholder.com/100" alt="Rakshitha" />
-            <h1 className='n3'> Rakshitha: Manages design and user experience to keep Connectify visually appealing and user-friendly. Email at </h1>
-            <a  className='e3' href="mailto:rakshitha@connectify.com"> rakshitha@connectify.com</a>.
-         
-            <img className='img-4' src="https://via.placeholder.com/100" alt="Suhani" />
-            <h1 className='n4'>Suhani: Drop an email at </h1>
-            <a  className='e4' href="mailto:suhani@connectify.com"> suhani@connectify.com</a>.
+        <div className='profile-card-1'>
+            <img className='profile-card' src="https://via.placeholder.com/100" alt="Preethi" />
+            <h3 className='profile-name'>Preethi NR </h3>
+            <span className="profile-description">Reach out at</span>
+            <a className='profile-email' href="mailto:preethi@connectify.com"> preethi@connectify.com</a>
+        </div>   
+        <div className='profile-card-2'>
+            <img className='profile-card' src="https://via.placeholder.com/100" alt="Puneeth" />
+            <h3 className='profile-name'>Puneeth:  </h3>
+            <span className="profile-description">Contact at</span>
+            <a  className='profile-email' href="mailto:puneeth@connectify.com"> puneeth@connectify.com</a>.
+        </div> 
+        <div className='profile-card-3'>
+            <img className='profile-card' src="https://via.placeholder.com/100" alt="Rakshitha" />
+            <h3 className='profile-name'> Rakshitha:  </h3>
+            <span className="profile-description" > Email at</span>
+            <a  className='profile-email' href="mailto:rakshitha@connectify.com"> rakshitha@connectify.com</a>.
+         <div className='profile-card-4'>
+            <img className='profile-card' src="https://via.placeholder.com/100" alt="Suhani" />
+            <h3 className='profile-name'>Suhani: </h3>
+            <span className="profile-description">Drop an email at </span>
+            <a  className='profile-email' href="mailto:suhani@connectify.com"> suhani@connectify.com</a>.
         
-        </ul>
+        </div>
+      </div>
         <p className='email'>
           General inquiries? Email us at 
           <a href="mailto:support@connectify.com"> support@connectify.com</a>. 
